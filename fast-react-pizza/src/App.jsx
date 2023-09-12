bf239fc8d5ea4abba9f829a79b0c78daf34dc8e4
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-09-12 01:05:51 -0400

    setting up routes and functionality

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
+]);
 
 export default function App() {
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router} />;
 }
