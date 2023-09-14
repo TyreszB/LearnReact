a67d62ab8ae14fbecf16c2673921d6c557872eb4
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-09-14 12:22:48 -0400

    added delete button to menu when item is added to cart

--- fast-react-pizza/src/features/menu/MenuItem.jsx
+++ fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,12 +1,14 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
-import { useDispatch } from "react-redux";
-import { addItem } from "../cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
-
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const inCart = currentQuantity > 0;
 
   function handleAddToCart(e) {
     e.preventDefault();
@@ -40,7 +42,9 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {!soldOut && (
+          {inCart && <DeleteItem pizzaId={id} />}
+
+          {!soldOut && !inCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
             </Button>
