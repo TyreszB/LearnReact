9645e26694330d0e8de16224df140e59b049bd4b
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-09-14 11:53:17 -0400

    added functionality to clear cart

--- fast-react-pizza/src/features/cart/CartOverview.jsx
+++ fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -13,7 +13,7 @@ function CartOverview() {
     <div className="bg-stone-800 text-stone-200 uppercase py-4 px-4 sm:px-6 text-sm md:text-base flex justify-between items-center">
       <p className="font-semibold text-stone-300 space-x-4 sm:space-x-6">
         <span>{totalCartQuantitiy} pizzas</span>
-        <span>${formatCurrency(totalCartPrice)}</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
