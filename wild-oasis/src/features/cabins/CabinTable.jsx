bf125cba6f536c99b98aaa45860c7c810d5bdba4
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-09-22 13:44:51 -0400

    added filter options for the cabin tables

--- wild-oasis/src/features/cabins/CabinTable.jsx
+++ wild-oasis/src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import CabinRow from "./CabinRow";
 import useCabins from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 // const TableHeader = styled.header`
 //   display: grid;
@@ -23,9 +24,23 @@ import Menus from "../../ui/Menus";
 export default function CabinTable() {
   const { isLoading, cabins, error } = useCabins();
 
+  const [searchParams] = useSearchParams();
+
   if (error) return <div>{error.message}</div>;
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+
+  if (filterValue === "all") filteredCabins = cabins;
+
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
   return (
     <Menus>
@@ -39,7 +54,7 @@ export default function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
