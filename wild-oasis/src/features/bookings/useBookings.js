61054f2644d1e6a8b2526370d263cf7622b845b8
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-09-25 21:48:35 -0400

    sorted the bookings with more features

--- wild-oasis/src/features/bookings/useBookings.js
+++ wild-oasis/src/features/bookings/useBookings.js
@@ -12,13 +12,17 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue };
 
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryFn: () => getBookings({ filter }),
-    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy],
   });
 
   return { isLoading, error, bookings };
