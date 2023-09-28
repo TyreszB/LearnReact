2e3cf348d9f569a69a40c8a2ecfbe5616d01e48d
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-09-28 00:28:58 -0400

    grabbed and computed data from supabase to later display charts on dashboard

--- wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import useRecentBookings from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import useRecentStays from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,19 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  const { bookings, isLoading1 } = useRecentBookings();
+  console.log(bookings);
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+  if (isLoading1 || isLoading2) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </StyledDashboardLayout>
+  );
+}
